e7f7d0ee9d3580aac3d2e98deb62735615d9a97a
Robert Pecz <rob.pecz@gmail.com>
2018-05-03 11:05:03 +0200

    Make TS001, TS002, TS003

--- cypress/integration/SignUpPositive.js
+++ cypress/integration/SignUpPositive.js
@@ -1,30 +1,40 @@
-describe('Login test functionality of the Phptravels page', function () {
+describe('Login test functionality (positive) of the Phptravels page', function () {
     beforeEach(function () {
         cy.viewport(1920, 1080);
         cy.visit('https://www.phptravels.net/');
     });
-    context('Login function positive scenario', function () {
+    context('Sign Up function positive scenario', function () {
         it('TC002',function () {
+            // Click on the My account dropdown list
             cy.get('.col-sm-8 > .navbar-side > #li_myaccount > .dropdown-toggle')
-                .click();        
+                .click();   
+            // Select the Sign up element
             cy.get('.col-sm-8 > .navbar-side > #li_myaccount > .dropdown-menu > :nth-child(2) > .go-text-right')
                 .click();
+            // Give a random string to the first name
             var firstName = makeid(6, true, false, false);
             cy.get('#headersignupform > :nth-child(3) > .form-control')
                 .type(firstName);
+            // Give a random string to the last name
             var lastName = makeid(6, true, false, false);
             cy.get(':nth-child(4) > .form-control')
                 .type(lastName);
+            // Give a random number to the mobile number
             cy.get(':nth-child(5) > .form-control')
                 .type(makeid(11, false, true, false));
+            // Give a random string to the email field in the next format string@string.com
             cy.get(':nth-child(6) > .form-control')
                 .type(makeid(5, false, false, true));
+            // Give the password 123456
             cy.get(':nth-child(7) > .form-control')
                 .type('123456');
+            // Confirm the password
             cy.get(':nth-child(8) > .form-control')
                 .type('123456');
+            // Click on the sign up button
             cy.get('.signupbtn')
                 .click();
+            // Assert the url and the name field on the new page
             cy.url()
                 .should('eq','https://www.phptravels.net/account/');
             cy.get('.col-md-6.go-right > .RTL')              
@@ -32,6 +42,7 @@ describe('Login test functionality of the Phptravels page', function () {
                 .and('contain', lastName);                    
         });
     });
+    // Random string/number generator
     function makeid(number, isName, isMobile, isEmail) {
         var text = "";
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
