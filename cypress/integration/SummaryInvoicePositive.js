a981e9bbbf074511975235a7fa01b5ea0fe0bd37
Robert Pecz <rob.pecz@gmail.com>
2018-05-09 10:19:51 +0200

    Adding TS002 step1 and step2

--- cypress/integration/SummaryInvoicePositive.js
+++ cypress/integration/SummaryInvoicePositive.js
@@ -37,7 +37,7 @@ describe('Summary Invoice functionality test (positive) of the Phptravels page',
             cy.url()
                 .should('contain', 'Dubai');
             // Assert that the top blue ribbon containing the Dubai word
-            cy.get('.mob-trip-info-head > span')
+            cy.get('.mob-trip-info-head > span', { timeout: 10000 })
                 .should('contain', 'Dubai');
             // Assert that the top blue ribbon containing the check out date
             cy.get('.mob-trip-info-head > div')
@@ -46,7 +46,7 @@ describe('Summary Invoice functionality test (positive) of the Phptravels page',
             cy.get(':nth-child(1) > .wow > .col-md-8 > :nth-child(1) > .mt0 > a > b')
                 .click();
             // Scroll to the first result of the avaiable rooms and click on the first result
-            cy.get(':nth-child(1) > .wow > .col-md-8 > :nth-child(6) > .col-md-6 > .btn')
+            cy.get(':nth-child(1) > .wow > .col-md-8 > :nth-child(1) > :nth-child(7) > .col-md-6 > .btn', { timeout: 10000 })
                 .scrollIntoView()
                 .click();
             // Type the first name
@@ -57,10 +57,10 @@ describe('Summary Invoice functionality test (positive) of the Phptravels page',
                 .type('Pecz');
             // Type the email
             cy.get(':nth-child(3) > .form-group > .form-control')
-                .type('test@test.com');
+                .type('testRandom@test.com');
             // Type the confirm email
             cy.get('#guestform > :nth-child(4) > .form-group > .form-control')
-                .type('test@test.com');
+                .type('testRandom@test.com');
             // Type the mobile number
             cy.get(':nth-child(6) > .form-group > .form-control')
                 .type('1234567890');
@@ -83,6 +83,119 @@ describe('Summary Invoice functionality test (positive) of the Phptravels page',
             // Assert that the user is on the invoice page
             cy.get('[style="font-size: 34px;text-transform:uppercase;font-weight: bold;"]', { timeout: 10000 })
                 .should('text', 'Invoice');
+        });
+        it('TS002, Booking Summary with extras and without coupon', function () {
+            // Click on the location textbox
+            cy.get('#s2id_autogen10 > .select2-choice > .select2-chosen')
+                .click();
+            // On the textfield, type Dubai
+            cy.get('#select2-drop > .select2-search > .select2-input')
+                .type('Dubai');
+            // Click on the suggested result
+            cy.get('.select2-results-dept-1 > .select2-result-label')
+                .click();
+            // Modify the today date, adding one month to it
+            var d = new Date();
+            var caller = require('C:\\Users\\robert.pecz\\Documents\\CypressProjects\\PhpTravels\\cypress\\SupportJs\\DateModifier');
+            var dateCaller = caller.ModifyDate(1, d);
+            // Clearing the date field and type the modified date into the Check in datefield
+            cy.get('#dpd1 > .form')
+                .clear()
+                .type(dateCaller)
+                .click();
+            // Modify the today date, adding two month to it
+            dateCaller = caller.ModifyDate(2, d);
+            // Clearing the date field and type the modified date into the Check out datefield
+            cy.get('#dpd2 > .form')
+                .clear()
+                .type(dateCaller)
+                .click();
+            // Click on the search button
+            cy.get(':nth-child(3) > .col-md-12 > .btn-danger')
+                .click();
+            // Assert that the url containing the Dubai word
+            cy.url()
+                .should('contain', 'Dubai');
+            // Assert that the top blue ribbon containing the Dubai word
+            cy.get('.mob-trip-info-head > span')
+                .should('contain', 'Dubai');
+            // Assert that the top blue ribbon containing the check out date
+            cy.get('.mob-trip-info-head > div')
+                .should('contain', dateCaller);
+            // Click on the first result
+            cy.get(':nth-child(1) > .wow > .col-md-8 > :nth-child(1) > .mt0 > a > b')
+                .click();
+            // Scroll to the first result of the avaiable rooms and click on the first result
+            cy.get(':nth-child(1) > .wow > .col-md-8 > :nth-child(1) > :nth-child(7) > .col-md-6 > .btn', { timeout: 10000 })
+                .scrollIntoView()
+                .click();
+            // Type the first name
+            cy.get('#guestform > :nth-child(1) > .form-group > .form-control')
+                .type('Robert');
+            // Type the last name
+            cy.get('#guestform > :nth-child(2) > .form-group > .form-control')
+                .type('Pecz');
+            // Type the email
+            cy.get(':nth-child(3) > .form-group > .form-control')
+                .type('testRandom@test.com');
+            // Type the confirm email
+            cy.get('#guestform > :nth-child(4) > .form-group > .form-control')
+                .type('testRandom@test.com');
+            // Type the mobile number
+            cy.get(':nth-child(6) > .form-group > .form-control')
+                .type('1234567890');
+            // Type the address
+            cy.get(':nth-child(7) > .form-group > .form-control')
+                .type('Random street 1');
+            // Select Hungary from the country dropdown list
+            cy.get('.select2-chosen')
+                .click();
+            cy.get(':nth-child(78) > .select2-result-label')
+                .scrollIntoView()
+                .click();
+            // Type random string into the Notes / Additional field
+            cy.get(':nth-child(10) > .form-group > .form-control')
+                .type('Random random random random random random random random random done.');
+            // Assert if the user add one extra its appear on the items list an adding the price to the Total Amount
+            // Put amount object into a const
+            cy.get('#displaytotal').then(($amount) => {
+                const totalAmount = $amount.text();
+                cy.get(':nth-child(1) > :nth-child(4) > .switch-light > :nth-child(2) > :nth-child(2)')
+                    .scrollIntoView()
+                    .click();
+                // Assert that the Total amount is changed
+                cy.get('#displaytotal').should(($changedAmount) => {
+                    expect($changedAmount.text()).not.to.eq(totalAmount);                                      
+                });
+            });
+            // Put the extra name in a const
+            cy.get(':nth-child(1) > :nth-child(2) > .item_cart').then(($name) => {
+                const nameOfTheExtra = $name.text();
+                // Assert that the name is on the amounts list
+                cy.get('.allextras > :nth-child(1)').should(($extraInTheAmountList) => {
+                    expect($extraInTheAmountList.text()).to.eq(nameOfTheExtra);
+                });
+            });
+            //Assert if clicking the same extra it's disappear from the invoice list and deducted from the Total amount
+            // Put amount object into a const
+            cy.get('#displaytotal').then(($amount) => {
+                const totalAmount = $amount.text();
+                cy.get(':nth-child(1) > :nth-child(4) > .switch-light > :nth-child(2) > :nth-child(2)')
+                    .scrollIntoView()
+                    .click();
+                // Assert that the Total amount is changed
+                cy.get('#displaytotal').should(($changedAmount) => {
+                    expect($changedAmount.text()).not.to.contains(totalAmount);
+                });
+            });
+            /*
+            // Click on the confrim booking button
+            cy.get(':nth-child(7) > .btn')
+                .scrollIntoView()
+                .click();
+            // Assert that the user is on the invoice page
+            cy.get('[style="font-size: 34px;text-transform:uppercase;font-weight: bold;"]', { timeout: 10000 })
+                .should('text', 'Invoice');*/
         });
     });
 });
